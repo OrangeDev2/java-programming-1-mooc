122190323d703935ab01a6e232c0d1889f409ea5
OrangeDev2 <47803678+OrangeDev2@users.noreply.github.com>
2022-04-25 00:56:58 -0700

    Added exericses.

---
+++ Part 1/Part01_03.OnceUponATime.java
@@ -0,0 +1,11 @@
+public class OnceUponATime {
+
+    public static void main(String[] args) {
+        // Write your program here.  Type "sout" and press tab.  That will convert to System.out.println()!
+
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+
+    }
+}
